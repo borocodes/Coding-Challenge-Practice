7cf2a83594356c48ac1f8441eb25c4e87a38d9fb
Jonathan Borofsky <jonborofsky@gmail.com>
2022-04-08 13:28:45 -0500

    maxChar completed

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -6,17 +6,26 @@
 // maxChar("apple 1231111") === "1"
 
 function maxChar(str) {
-  const chars = {};
+  const charMap = {}
+  let max = 0;
+  let maxChar = '';
 
-  for (let char of str) {
-    if (!chars[char]) {
-      chars[char] = 1;
+  for ( let char of str) {
+    if (charMap[char]) {
+      charMap[char]++
     } else {
-      chars[char]++;
+      charMap[char] = 1;
     }
   }
-  for (let key in chars) {
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char
+    }
   }
+
+  return maxChar
 }
 
 module.exports = maxChar;
