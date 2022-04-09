bebd27a904c751186414cf5511ad5dfff08b41e5
Jonathan Borofsky <jonborofsky@gmail.com>
2022-04-09 14:40:58 -0500

    chunk completed

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,41 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+//solution using for...of loop
+/*
+function chunk(array, size) {
+  //let chunked = []
+  //for (let el of array)
+  //if !chunked.length -1 || chunked.length == size
+  //chunked.push(el)
+  //else array.push(el)
+
+  let chunked = [];
+
+  for (let el of array) {
+    let last = chunked[chunked.length - 1];
+    if (!last || last.length == size) {
+      chunked.push([el]);
+    } else {
+      last.push(el);
+    }
+  }
+
+  return chunked;
+}
+*/
+
+//using slice() method
+function chunk(array, size) {
+  let chunked = [];
+  idx = 0;
+
+  while (idx < array.length) {
+    let sliced = array.slice(idx, idx + size);
+    chunked.push(sliced);
+    idx += size;
+  }
+  return chunked;
+}
 
 module.exports = chunk;
