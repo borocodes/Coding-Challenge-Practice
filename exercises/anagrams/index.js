9cb6ce3e4059b9dccee479355805e4f65ae4ff16
Jonathan Borofsky <jonborofsky@gmail.com>
2022-04-11 18:27:58 -0500

    alt anagram answers + fixing anagram tests

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,15 +8,71 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-//my solution: convert strings into arrays, sort, and join back together as a string. Then, compare the sorted strings and return true. Otherwise, return false.
+//my solution: convert strings into arrays, sort, and join back together as a string. Then, compare the sorted strings and return true. Otherwise, return false. Passes all tests but the solution nor tests are accounting for edge cases mentioned in problem description
 function anagrams(stringA, stringB) {
   let arrayA = stringA.split('').sort().join('');
+
   let arrayB = stringB.split('').sort().join('');
 
   if (arrayA === arrayB) {
     return true;
   }
+
   return false;
 }
 
 module.exports = anagrams;
+
+//ALTERNATIVE SOLUTIONS
+
+// solution similar to mine but actually accounts for special characters, white space, etc. Makes use of a helper function
+//--------------------------------------------------------------------
+/*
+function anagrams(stringA, stringB) {
+  const cleanA = strCleanUp(stringA)
+  const cleanB = strCleanUp(stringB)
+
+  if (cleanA === cleanB) {
+    return true
+  }
+
+  return false;
+}
+
+//helper function
+function strCleanUp(str) {
+  return str.replace(/[^\w]/g).toLowerCase().split('').sort().join('')
+}
+*/
+//--------------------------------------------------------------------
+
+//solution using a character map w/helper function and regex to account for edge cases
+
+/*----------------------------------------------------------------------------
+function anagrams(stringA, stringB) {
+  const aCharMap = buildCharMap(stringA);
+  const bCharMap = buildCharMap(stringB);
+
+  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+    return false;
+  }
+
+  for (let char in aCharMap) {
+    if (aCharMap[char] !== bCharMap[char]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+\\helper function for character map solution
+function buildCharMap(str) {
+  const charMap = {};
+  
+  for (let char of str.replace(/[^\w]/g).toLowerCase()) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+  return charMap;
+}
+------------------------------------------------------------------------------*/
--- exercises/anagrams/test.js
+++ exercises/anagrams/test.js
@@ -8,7 +8,7 @@ test('"hello" is an anagram of "llohe"', () => {
   expect(anagrams('hello', 'llohe')).toBeTruthy();
 });
 
-test('"Whoa! Hi!" is an anagram of "Hi! Whoa!"', () => {
+test('"Whoa! Hi!" is an anagram of "Hi Whoa"', () => {
   expect(anagrams('Whoa! Hi!', 'Hi! Whoa!')).toBeTruthy();
 });
 
